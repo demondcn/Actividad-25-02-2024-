53568c1c6bab9ebc3668c416ac14862ac8118398
Cristian <guerrero70407@gmail.com>
2024-02-19 07:37:47 -0500

    Creaccion de la clase Complejo, con sus artributos y constructores de los mismos

--- src/Complejo.java
+++ src/Complejo.java
@@ -1,5 +1,15 @@
 import javax.swing.*;
 
 public class Complejo {
+    private double real;
+    private double imaginary;
 
+    //constructor
+    public Complejo(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    //metodos del objeto para realizar las respectivas Operaciones aritmeticas
+    
 }
