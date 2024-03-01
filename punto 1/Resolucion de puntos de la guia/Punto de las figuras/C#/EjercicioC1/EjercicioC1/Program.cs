62e2d9a9781e7220833133f1c3f137047cbec701
Cristian <guerrero70407@gmail.com>
2024-02-29 21:53:48 -0500

    Reorganizacion de Archivos y implementacion de archivo c# del codigo de carlos y el de cristian

---
+++ punto 1/Resolucion de puntos de la guia/Punto de las figuras/C#/EjercicioC1/EjercicioC1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+namespace Figuras
+{
+
+	class Program
+	{
+
+    	   static void Main(string[] args)
+  	   {
+            Console.WriteLine("*********     ***       *       *");
+            Console.WriteLine("*       *   *     *    ***     * *");
+            Console.WriteLine("*       *  *       *  *****   *   *");
+            Console.WriteLine("*       *  *       *    *    *     *");
+            Console.WriteLine("*       *  *       *    *   *       *");
+            Console.WriteLine("*       *  *       *    *    *     * ");
+            Console.WriteLine("*       *   *     *     *     *   *");
+            Console.WriteLine("*********     ***       *       *");
+	    }
+        }
+    
+}
